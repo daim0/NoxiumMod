81d2f98e6e199e38ad288cfe2ec4c523317306b4
daim0 <59332742+daim0@users.noreply.github.com>
2020-02-09 14:54:46 +0000

    Add files via upload

---
+++ Projectiles/CrystalBoomer.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Projectiles
+{
+    class CrystalBoomer : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 78;
+            projectile.height = 78;
+            projectile.aiStyle = 3;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.penetrate = 10;
+            projectile.extraUpdates = 1;
+            projectile.tileCollide = false;
+        }
+    }
+}
