eeec0b76559f539f3ad2cf29782e781fb6df6407
daim0 <59332742+daim0@users.noreply.github.com>
2020-02-22 13:42:47 +0000

    a

---
+++ Projectiles/Wooden/PureWoodArrowP.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace NoxiumMod.Projectiles.Wooden
+{
+    class PureWoodArrowP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 14;
+            projectile.height = 24;
+            projectile.CloneDefaults(ProjectileID.WoodenArrowFriendly);
+            aiType = ProjectileID.WoodenArrowFriendly;
+        }
+    }
+}
---
+++ Projectiles/Wooden/PureWoodKnifeP.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace NoxiumMod.Projectiles.Wooden
+{
+    class PureWoodKnifeP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 12;
+            projectile.height = 26;
+            projectile.CloneDefaults(ProjectileID.ThrowingKnife);
+            aiType = ProjectileID.ThrowingKnife;
+        }
+    }
+}
