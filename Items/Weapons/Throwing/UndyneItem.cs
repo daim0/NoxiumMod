d53b67e5160ac3e8af615b775e0bde5823deb36b
daim0 <59332742+daim0@users.noreply.github.com>
2020-03-17 15:29:01 +0000

    Add files via upload

---
+++ Items/Weapons/Throwing/UndyneItem.cs
@@ -0,0 +1,54 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System.ComponentModel;
+using System;
+using NoxiumMod.Projectiles.Throwing;
+
+namespace NoxiumMod.Items.Weapons.Throwing
+{
+    class UndyneItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spear of Justice");
+        }
+        public override void SetDefaults()
+        {
+            item.shootSpeed = 10f;
+            item.damage = 15;
+            item.knockBack = 5f;
+            item.useStyle = 1;
+            item.useAnimation = 12;
+            item.useTime = 12;
+            item.width = 64;
+            item.height = 64;
+            item.maxStack = 1;
+            item.rare = 2;
+
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.autoReuse = true;
+            item.thrown = true;
+
+            item.UseSound = SoundID.Item1;
+            item.value = Item.sellPrice(silver: 100);
+            item.shoot = 1;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+
+            float Angle = MathHelper.ToRadians(Main.rand.NextFloat(0, 360));
+            float distance = 190;
+            float pX = distance * (float)Math.Cos(Angle) + Main.MouseWorld.X;
+            float pY = distance * (float)Math.Sin(Angle) + Main.MouseWorld.Y;
+            Vector2 thing = new Vector2(pX, pY);
+
+            Projectile.NewProjectile(pX, pY, 0f, 0f, mod.ProjectileType("UndyneP"), damage, 1f, player.whoAmI);
+            return false;
+
+        }
+    }
+}
