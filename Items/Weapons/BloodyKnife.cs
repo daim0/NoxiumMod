aa873aca460f067a1c98698c29c9631a54c975f7
Lemmy-Koopa <56778754+Lemmy-Koopa@users.noreply.github.com>
2020-03-18 16:19:59 -0400

    Add files via upload

---
+++ Items/Weapons/BloodyKnife.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using NoxiumMod.Projectiles;
+
+namespace NoxiumMod.Items.Weapons
+{
+	public class BloodyKnife : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.shootSpeed = 10f;
+			item.damage = 10;
+			item.knockBack = 5f;
+			item.useStyle = 1;
+			item.useAnimation = 17;
+			item.useTime = 17;
+			item.width = 30;
+			item.height = 30;
+			item.rare = 5;
+			item.consumable = false;
+			item.noUseGraphic = true;
+			item.noMelee = true;
+			item.autoReuse = true;
+			item.thrown = true;
+			item.UseSound = SoundID.Item1;
+			item.value = Item.sellPrice(silver: 5);
+			item.shoot = ProjectileType<BloodyKnifeProjectile>();
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.CrimtaneBar, 10);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+		}
+	}
+}
