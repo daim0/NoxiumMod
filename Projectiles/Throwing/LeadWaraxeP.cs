210dc9a79c539edb9e5c9f4f1a494735d508c0cd
daim0 <59332742+daim0@users.noreply.github.com>
2020-04-02 15:29:39 +0100

    ae

---
+++ Projectiles/Throwing/LeadWaraxeP.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using NoxiumMod.Items.Weapons.Throwing;
+using Terraria;
+using System;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Projectiles.Throwing
+{
+    class LeadWaraxeP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 48;
+            projectile.height = 60;
+            projectile.friendly = true;
+            projectile.thrown = true;
+            projectile.scale = 0.8f;
+            projectile.tileCollide = true;
+            projectile.penetrate = 1;
+        }
+        public override void AI()
+        {
+            float rotationsPerSecond = 3f;
+            projectile.rotation += MathHelper.ToRadians(rotationsPerSecond * 6f);
+            const float velXmult = 0.98f;
+            const float velYmult = 0.25f;
+            projectile.velocity.X *= velXmult;
+            projectile.velocity.Y += velYmult;
+        }
+    }
+}
