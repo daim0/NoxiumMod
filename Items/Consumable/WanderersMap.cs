a8cf2714ca81a5afe28d41473e7c5047dfb7ee8b
daim0 <59332742+daim0@users.noreply.github.com>
2020-04-08 18:05:34 +0100

    Add files via upload

---
+++ Items/Consumable/WanderersMap.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Items.Consumable
+{
+    class WanderersMap : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 26;
+            item.height = 32;
+            item.value = 1000;
+            item.useStyle = 4;
+            item.consumable = true;
+            item.rare = 3;
+        }
+
+        public override bool UseItem(Player player)
+        {
+            for(int xCoord= 0; xCoord < Main.maxTilesX; xCoord++)
+            {
+                for (int yCoord = 0; yCoord < Main.maxTilesX; yCoord++)
+                {
+                    if (WorldGen.InWorld(xCoord, yCoord, 0))
+                    {
+                        Main.Map.Update(xCoord, yCoord, 255);
+                    }
+                }
+            }
+            Main.refreshMap = true;
+            return true;
+        }
+    }
+}
