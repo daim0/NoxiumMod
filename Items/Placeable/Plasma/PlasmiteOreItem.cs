1b706399775fadd7d34521fbaf6a7ee1f1d456ad
daim0 <59332742+daim0@users.noreply.github.com>
2020-04-13 21:01:10 +0100

    Add files via upload

---
+++ Items/Placeable/Plasma/PlasmiteOreItem.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using NoxiumMod.Tiles.Plasma;
+
+namespace NoxiumMod.Items.Placeable.Plasma
+{
+    class PlasmiteOreItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Plasmite Ore");
+            ItemID.Sets.SortingPriorityMaterials[item.type] = 58;
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.Plasma.PlasmiteOre>();
+            item.width = 18;
+            item.height = 18;
+            item.value = 300;
+        }
+    }
+}
