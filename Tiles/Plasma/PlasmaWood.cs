0e8dede37ddaf4b370976baf348840801721eeca
daim0 <59332742+daim0@users.noreply.github.com>
2020-04-29 12:44:08 +0100

    Add files via upload

---
+++ Tiles/Plasma/PlasmaWood.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace NoxiumMod.Tiles.Plasma
+{
+    class PlasmaWood : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileValue[Type] = 410;
+            Main.tileMerge[Type][TileType<PlasmaSand>()] = true;
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Plasma Sandstone");
+            AddMapEntry(new Color(172, 153, 185), name);
+
+            dustType = 84;
+            drop = ItemType<Items.Placeable.Plasma.PlasmaWoodItem>();
+            soundType = 21;
+            soundStyle = 1;
+        }
+    }
+}
