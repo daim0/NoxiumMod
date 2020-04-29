59574019486c779e341383317439ee44780e79f6
daim0 <59332742+daim0@users.noreply.github.com>
2020-04-29 12:42:14 +0100

    Add files via upload

---
+++ Items/Materials/Chartreum.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Items.Materials
+{
+    class Chartreum : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 26;
+            item.maxStack = 999;
+            item.value = Item.buyPrice(silver: 5);
+        }
+    }
+}
