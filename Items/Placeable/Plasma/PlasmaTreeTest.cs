df51383b24f60e75d526f88b4ec6120df4c00269
daim0 <59332742+daim0@users.noreply.github.com>
2020-04-29 12:42:48 +0100

    Add files via upload

---
+++ Items/Placeable/Plasma/PlasmaTreeTest.cs
@@ -0,0 +1,25 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace NoxiumMod.Items.Placeable.Plasma
+{
+    class PlasmaTreeTest : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 24;
+            item.maxStack = 99;
+            item.value = 750;
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.Plasma.HugeTree>();
+            item.placeStyle = 0;
+        }
+    }
+}
