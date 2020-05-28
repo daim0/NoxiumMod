b1d9f56e6d087e4c8fac1d903461eedb41690b84
GoodPro712 <charliebrannan154@gmail.com>
2020-05-28 20:44:08 +1000

    holy fuck daim

--- UI/ahmBar.cs
+++ UI/ahmBar.cs
@@ -1,22 +1,12 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.UI;
-using Terraria.UI.Chat;
-using Terraria.GameContent.UI.Elements;
-using Terraria.DataStructures;
-
 
 namespace NoxiumMod.UI
 {
-    class AhmBar : UIState
+    public class AhmBar : UIState
     {
         public Texture2D BorderTexture;
         public Texture2D BarTexture;
@@ -28,6 +18,7 @@ namespace NoxiumMod.UI
             BarTexture = ModContent.GetTexture("NoxiumMod/UI/UI_Bar");
             BottomTexture = ModContent.GetTexture("NoxiumMod/UI/UI_Interior");
         }
+
         public override void Draw(SpriteBatch spriteBatch)
         {
             if (NoxiumWorld.ahmBarShown)
@@ -36,7 +27,7 @@ namespace NoxiumMod.UI
                 Vector2 ScreenPos = new Vector2(Main.screenWidth / 2 - BorderTexture.Width / 2 + 600, Main.screenHeight / 2 - 410);
                 //adjust the bar position relative to the frame
                 Vector2 BarAdjust = new Vector2(18, 54);
-                var length = ((NoxiumWorld.ahmTimer * BarTexture.Height) / NoxiumWorld.ahmTimerCap);
+                int length = ((NoxiumWorld.ahmTimer * BarTexture.Height) / NoxiumWorld.ahmTimerCap);
                 //Draw frame
                 spriteBatch.Draw(BottomTexture, ScreenPos + new Vector2(0, 2), null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
                 //Draw bar
