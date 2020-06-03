b729d7cd2c0f15529c8b672b341d320e3ffd7ed5
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-03 14:31:16 +0100

    I

---
+++ Items/Accesories/DreadfulChalice.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Items.Accesories
+{
+    class DreadfulChalice : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 40;
+            item.value = 1000;
+            item.useStyle = 4;
+            item.rare = 3;
+        }
+    }
+}
