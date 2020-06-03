c32c8f3634e77eccc394b7d7d5057ef69ea5b611
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-03 14:37:03 +0100

    Add files via upload

---
+++ Items/Weapons/Moonless/DireKunai.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Items.Weapons.Moonless
+{
+    class DireKunai : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 14;
+            item.height = 36;
+            item.value = 1000;
+            item.useStyle = 4;
+            item.rare = 3;
+        }
+    }
+}
---
+++ Items/Weapons/Moonless/Nystagmus.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Items.Weapons.Moonless
+{
+    class Nystagmus : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 20;
+            item.height = 34;
+            item.value = 1000;
+            item.useStyle = 4;
+            item.rare = 3;
+        }
+    }
+}
