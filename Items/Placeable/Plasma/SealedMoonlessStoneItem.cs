2df1579b85876b3b0bb90596545571e2f6199466
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-03 15:33:01 +0100

    aeae

---
+++ Items/Placeable/Plasma/SealedMoonlessStoneItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using NoxiumMod.Tiles.Plasma;
+
+namespace NoxiumMod.Items.Placeable.Plasma
+{
+    class SealedMoonlessStoneItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sealed Moonless Stone");
+            ItemID.Sets.SortingPriorityMaterials[item.type] = 58;
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 999;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.Plasma.SealedMoonlessStone>();
+            item.width = 14;
+            item.height = 16;
+            item.value = 300;
+        }
+    }
+}
