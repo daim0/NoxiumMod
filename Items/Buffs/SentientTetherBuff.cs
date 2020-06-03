b960e36327fd4e94b641939abee7da1600490838
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-03 14:32:09 +0100

    Add files via upload

---
+++ Items/Buffs/SentientTetherBuff.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace NoxiumMod.Items.Buffs
+{
+    class SentientTetherBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Sentient Tether Minion");
+            Description.SetDefault("A Sentient Tether fights for you!");
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            if (player.ownedProjectileCounts[mod.ProjectileType("Crawlerock")] > 0)
+            {
+                player.GetModPlayer<NoxiumPlayer>().SentientTetherMinion = false;
+            }
+
+            if (!player.GetModPlayer<NoxiumPlayer>().SentientTetherMinion)
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+                return;
+            }
+
+            player.buffTime[buffIndex] = 18000;
+        }
+    }
+}
