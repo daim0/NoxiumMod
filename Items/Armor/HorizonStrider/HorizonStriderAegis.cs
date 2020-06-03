1ce604305a5ded89e657534cca81528242ecfd78
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-03 14:31:37 +0100

    wanna

---
+++ Items/Armor/HorizonStrider/HorizonStriderAegis.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace NoxiumMod.Items.Armor.HorizonStrider
+{
+    class HorizonStriderAegis : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 20;
+            item.value = 1000;
+            item.useStyle = 4;
+            item.rare = 3;
+        }
+    }
+}
