65ac42ac12f4032b3dfb37951be81b78ee28e74f
daim0 <59332742+daim0@users.noreply.github.com>
2020-06-18 22:13:20 +0100

    Added NoxiumConfig

---
+++ NoxiumConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using Terraria;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace NoxiumMod
+{
+    public class Config : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+        [Label("Loading Screen Night Mode")]
+        [Tooltip("Changes the base colors of the loading screen.")]
+        public bool NightMode { get; set; }
+    }
+}
