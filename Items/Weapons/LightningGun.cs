de169f9bce365d0b425a7074c273b2189491a377
daim0 <59332742+daim0@users.noreply.github.com>
2020-07-04 21:52:41 +0100

    Add files via upload

---
+++ Items/Weapons/LightningGun.cs
@@ -0,0 +1,30 @@
+﻿using NoxiumMod.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace NoxiumMod.Items.Weapons
+{
+    class LightningGun : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.damage = 40;
+            item.noMelee = true;
+            item.magic = true;
+            item.channel = true; //Channel so that you can hold the weapon [Important]
+            item.mana = 5;
+            item.rare = ItemRarityID.Pink;
+            item.width = 28;
+            item.height = 30;
+            item.useTime = 20;
+            item.UseSound = SoundID.Item13;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.shootSpeed = 14f;
+            item.useAnimation = 20;
+            item.shoot = ProjectileType<LightningGunProj>();
+            item.value = Item.sellPrice(silver: 3);
+        }
+    }
+}
